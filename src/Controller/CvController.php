1e597aaa9ad82bee1a4b9a8580ffb1a779766888
Bersheny <fhezrghjsd4@live.fr>
2023-05-04 22:52:01 +0200

    1

--- src/Controller/CvController.php
+++ src/Controller/CvController.php
@@ -7,10 +7,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/cv')]
 class CvController extends AbstractController
 {
-    #[Route('/', name: 'app_cv_index')]
+    #[Route('/cv', name: 'app_cv_index')]
     public function index(TranslatorInterface $translator): Response
     {
         return $this->render('default/cv.html.twig', [
