6bd17f03b894db2854ecb31346e2a202b70077cd
Bersheny <fhezrghjsd4@live.fr>
2023-06-06 10:22:24 +0200

    1

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -57,3 +57,27 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+let mybutton = document.getElementById("toTopBtn");
+mybutton.style.display = "none";
+
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (
+		document.body.scrollTop > 20 ||
+		document.documentElement.scrollTop > 20
+	) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
+}
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
