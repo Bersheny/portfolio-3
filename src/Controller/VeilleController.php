64f02fb7c37130df8f461dd284f7ae9db60944b7
Bersheny <fhezrghjsd4@live.fr>
2024-05-22 22:11:12 +0200

    Veille

--- src/Controller/VeilleController.php
+++ src/Controller/VeilleController.php
@@ -12,18 +12,46 @@ class VeilleController extends AbstractController
     #[Route('/veille-technologique', name: 'app_veille_index')]
     public function index(TranslatorInterface $translator): Response
     {
-        // $simpleXmlElement = simplexml_load_file('https://openai.com/blog/rss.xml');
-        $simpleXmlElement = simplexml_load_file('https://blogs.nvidia.com/feed/');
-        // $simpleXmlElement = simplexml_load_file('https://blog.google/technology/ai/rss/');
+        $simpleXmlElement1 = simplexml_load_file('https://aws.amazon.com/fr/blogs/machine-learning/feed/');
+        $simpleXmlElement2 = simplexml_load_file('https://blogs.nvidia.com/feed/');
 
-        //dump(print_r($simpleXmlElement->channel->item, true));
-        dump($simpleXmlElement->channel->item);
+        $items1 = $this->processRssItems($simpleXmlElement1->channel->item);
+        $items2 = $this->processRssItems($simpleXmlElement2->channel->item);
 
-        $datePublication = date('d/m/Y', strtotime((string) $simpleXmlElement->channel->item->pubDate));
+        return $this->render('default/veille-technologique.html.twig', [
+            'items1' => $items1,
+            'items2' => $items2,
+        ]);
+    }
 
+    private function processRssItems($rssItems)
+    {
+        $processedItems = [];
 
-        return $this->render('default/veille-technologique.html.twig', array(
-            'items' => $simpleXmlElement->channel->item,
-        ));
+        foreach ($rssItems as $item) {
+            // Extract and format the publication date
+            $datePublication = date_create_from_format('D, d M Y H:i:s O', (string)$item->pubDate);
+            $dateFormatted = $datePublication ? $datePublication->format('j F Y') : '';
+
+            $enclosureUrl = (string)$item->enclosure['url'];
+
+            // Attempt to get the author from <author> or <dc:creator>
+            $author = (string)$item->author;
+            if (empty($author)) {
+                $dc = $item->children('http://purl.org/dc/elements/1.1/');
+                $author = (string)$dc->creator;
+            }
+
+            $processedItems[] = [
+                'title' => (string)$item->title,
+                'link' => (string)$item->link,
+                'pubDate' => $dateFormatted, // Use formatted date
+                'author' => $author,
+                'description' => (string)$item->description,
+                'enclosureUrl' => $enclosureUrl
+            ];
+        }
+
+        return $processedItems;
     }
 }
